9cfd6e972af733cfbe576e5d393bfdacf43dcecc
a1excoder <aexnogreen@gmail.com>
2023-04-02 04:21:29 +0300

    update bot

---
+++ Data.py
@@ -0,0 +1,47 @@
+import os.path
+import sqlite3
+
+
+class Data:
+    def __init__(self, db_path):
+
+        if not os.path.isfile(db_path):
+            self.__connection = sqlite3.connect(db_path, check_same_thread=False)
+            self.__cursor = self.__connection.cursor()
+
+            self.__cursor.execute("""CREATE TABLE articles
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                    chat_id INTEGER, 
+                    text TEXT)
+                """)
+            self.__connection.commit()
+        else:
+            self.__connection = sqlite3.connect(db_path, check_same_thread=False)
+            self.__cursor = self.__connection.cursor()
+
+    def add(self, chat_id, string):
+        query = self.__cursor.execute("SELECT * from articles where chat_id=? AND text=?", (chat_id, string))
+        if len(query.fetchall()) == 0:
+            self.__cursor.execute("INSERT INTO articles (chat_id, text) VALUES (?, ?)", (chat_id, string))
+            self.__connection.commit()
+            return True
+        else:
+            return False
+
+    def list(self, chat_id):
+        query = self.__cursor.execute(f"SELECT id, text from articles where chat_id={chat_id}")
+        return query.fetchall()
+
+    def delete(self, chat_id, record_id):
+        query = self.__cursor.execute("DELETE from articles where chat_id=? AND id=?", (chat_id, record_id))
+        self.__connection.commit()
+
+        if query.rowcount == 0:
+            return False
+
+        return True
+
+    def __del__(self):
+        self.__cursor.close()
+        self.__connection.close()
+
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import threading
 from random import randint
 import telebot
 import demapi
-import json
-import sys
+from os import environ as env
+import Data
 
-API_KEY = sys.argv[1]
+API_KEY = env.get('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
-file = open('messages.json', mode='r', encoding='utf-8')
-messages = json.load(file)
-file.close()
+lock = threading.Lock()
+db = Data.Data("./database.sqlite")
 
 
 @bot.message_handler(content_types=['photo'])
@@ -23,12 +23,65 @@ def photo_worker(message):
             bot.send_photo(message.chat.id, conf.download().content, reply_to_message_id=message.message_id)
 
     elif randint(1, 100) >= 75:
+        lock.acquire(True)
+        values_list = db.list(message.chat.id)
+        lock.release()
+
         conf = demapi.Configure(
             base_photo=bot.download_file(bot.get_file(message.photo[len(message.photo) - 1].file_id).file_path),
-            title=messages['messages'][randint(0, len(messages['messages']) - 1)]
+            title=values_list[randint(0, len(values_list) - 1)]
         )
         bot.send_photo(message.chat.id, conf.download().content, reply_to_message_id=message.message_id)
 
 
-if __name__ == '__main__':
-    bot.polling()
+@bot.message_handler(commands=['add'])
+def add_to_list(message):
+    # message.from_user.id in bot.get_chat_administrators(message.chat.id)
+    status = bot.get_chat_member(message.chat.id, message.from_user.id).status
+    if status == "administrator" or status == "creator":
+        lock.acquire(True)
+        if db.add(message.chat.id, message.text[5:]):
+            bot.send_message(message.chat.id, f"'{message.text[5:]}' was added to the list",
+                             reply_to_message_id=message.message_id)
+        else:
+            bot.send_message(message.chat.id, f"'{message.text[5:]}' already on the list",
+                             reply_to_message_id=message.message_id)
+        lock.release()
+    else:
+        bot.send_message(message.chat.id, "you do not have enough permissions for this command",
+                         reply_to_message_id=message.message_id)
+
+
+@bot.message_handler(commands=['delete'])
+def delete_from_list(message):
+    status = bot.get_chat_member(message.chat.id, message.from_user.id).status
+    if status == "administrator" or status == "creator":
+
+        lock.acquire(True)
+        if db.delete(message.chat.id, message.text[8:]):
+            bot.send_message(message.chat.id, f"'{message.text[8:]}' was delete from the list",
+                             reply_to_message_id=message.message_id)
+        else:
+            bot.send_message(message.chat.id, f"'{message.text[8:]}' was not removed from the list",
+                             reply_to_message_id=message.message_id)
+
+        lock.release()
+    else:
+        bot.send_message(message.chat.id, "you do not have enough permissions for this command",
+                         reply_to_message_id=message.message_id)
+
+
+@bot.message_handler(commands=['list'])
+def view_list(message):
+    lock.acquire(True)
+    values_list = db.list(message.chat.id)
+    lock.release()
+
+    if len(values_list) == 0:
+        bot.send_message(message.chat.id, "list for this chat is empty", reply_to_message_id=message.message_id)
+    else:
+        bot.send_message(message.chat.id, '\n'.join([f"{value[0]} - '{value[1]}'" for value in values_list]), reply_to_message_id=message.message_id)
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
