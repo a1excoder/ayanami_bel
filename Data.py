0d95caeb8121f499927392078607a3ec5c4fb4e7
a1excoder <aexnogreen@gmail.com>
2023-07-15 01:38:45 +0300

    update

--- Data.py
+++ Data.py
@@ -1,26 +1,20 @@
-import os.path
 import sqlite3
 
 
 class Data:
     def __init__(self, db_path):
+        self.__connection = sqlite3.connect(db_path, check_same_thread=False)
+        self.__cursor = self.__connection.cursor()
 
-        if not os.path.isfile(db_path):
-            self.__connection = sqlite3.connect(db_path, check_same_thread=False)
-            self.__cursor = self.__connection.cursor()
-
-            self.__cursor.execute("""CREATE TABLE articles
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT,  
-                    chat_id INTEGER, 
-                    text TEXT)
-                """)
-            self.__connection.commit()
-        else:
-            self.__connection = sqlite3.connect(db_path, check_same_thread=False)
-            self.__cursor = self.__connection.cursor()
+        self.__cursor.execute("""CREATE TABLE IF NOT EXISTS articles
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                            chat_id INTEGER, 
+                            text TEXT)
+                        """)
+        self.__connection.commit()
 
     def add(self, chat_id, string):
-        query = self.__cursor.execute("SELECT * from articles where chat_id=? AND text=?", (chat_id, string))
+        query = self.__cursor.execute("SELECT id FROM articles WHERE chat_id=? AND text=?", (chat_id, string))
         if len(query.fetchall()) == 0:
             self.__cursor.execute("INSERT INTO articles (chat_id, text) VALUES (?, ?)", (chat_id, string))
             self.__connection.commit()
@@ -29,11 +23,12 @@ class Data:
             return -1
 
     def list(self, chat_id):
-        query = self.__cursor.execute(f"SELECT id, text from articles where chat_id=? order by id DESC", (chat_id))
+        query = self.__cursor.execute("SELECT id, text FROM articles WHERE chat_id=? ORDER BY id DESC", (chat_id, ))
         return query.fetchall()
 
+
     def delete(self, chat_id, record_id):
-        query = self.__cursor.execute("DELETE from articles where chat_id=? AND id=?", (chat_id, record_id))
+        query = self.__cursor.execute("DELETE FROM articles WHERE chat_id=? AND id=?", (chat_id, record_id))
         self.__connection.commit()
 
         if query.rowcount == 0:
@@ -42,8 +37,8 @@ class Data:
         return True
 
     def edit(self, chat_id, record_id, upd_data):
-        self.__cursor.execute(f"UPDATE articles SET text=? "
-                              f"WHERE chat_id =? AND id =?", (upd_data, chat_id, record_id))
+        self.__cursor.execute("UPDATE articles SET text=? "
+                              "WHERE chat_id =? AND id =?", (upd_data, chat_id, record_id))
         self.__connection.commit()
 
         if self.__cursor.rowcount == 0:
